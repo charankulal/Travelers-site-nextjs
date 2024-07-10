719c6b51843378205daa103d8523a391498be62e
Charan <charankulal0241@gmail.com>
2024-07-10 12:42:45 +0530

    Completed Hero and Camp Section.

---
+++ components/Button.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+import React from 'react'
+
+type ButtonProps = {
+    type: 'button' | 'submit';
+    title: string;
+    icon?: string;
+    variant: string;
+
+}
+
+const Button = ({ type, title, icon, variant }: ButtonProps) => {
+    return (
+        <button type={type} className={`flexCenter gap-3 rounded-full border ${variant}`} >{icon && <Image src={icon} alt={title} height={20} width={20} />}
+            <label className='bold-16 whitespace-nowrap'>{title}</label>
+        </button>
+    )
+}
+
+export default Button
---
+++ components/GetApp.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GetApp = () => {
+  return (
+    <div>
+      Get APp
+    </div>
+  )
+}
+
+export default GetApp
